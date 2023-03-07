aa578e8bc4ead967459b18ad8c808d6541e562a0
qmandev <qiangma7@gmail.com>
2023-03-07 17:03:18 -0500

    Initial Commit

---
+++ CircleAnimation/CircleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAnimationApp.swift
+//  CircleAnimation
+//
+//  Created by ARMSTRONG on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
